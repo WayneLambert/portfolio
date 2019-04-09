1bfd7190b90d4d37f5826d24b5989587f01d035d
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-04-09 21:23:33 +0100

    Add users, projects and owners. Add to admin panel

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Todo, Project, Owner
 
-# Register your models here.
+admin.site.register(Todo)
+admin.site.register(Project)
+admin.site.register(Owner)
