cb3a98021dfc636a8a00c412d264c6f114b883cb
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-04-11 20:44:56 +0100

    Add models.py to define my Post class/table

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
