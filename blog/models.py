42ef33fe59e81ad0ec5869e85a4dd318637e2698
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-05-28 20:29:34 -0100

    Adjust templates for images

--- blog/models.py
+++ blog/models.py
@@ -44,9 +44,3 @@ class Comment(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.body
-
-    def __repr__(self):
-        return self.body
--- blog/views.py
+++ blog/views.py
@@ -45,7 +45,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'body']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -54,7 +54,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'body']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
