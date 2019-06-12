b09db997614bbaf3516d26720fe490d976a74bed
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-06-12 20:03:46 +0100

    Add favicon

--- ab_back_end/settings.py
+++ ab_back_end/settings.py
@@ -191,9 +191,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ()
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
