691100f2375a7c3e9d557c38e87a5f7974df5720
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-06-24 20:38:08 +0100

    Further polish to portfolio

--- wordcount/views.py
+++ wordcount/views.py
@@ -8,10 +8,11 @@ def check_wordcount(request):
 
 
 def count(request):
+    ALPHABET = "abcdefghijklmnopqrstuvwxyz"
     word_list = []
     orig_full_text = request.GET['fulltext']
-    context = re.sub('[!@#.,/\';]', '', orig_full_text).lower()
-    word_list = context.split()
+    cleaned_full_text = re.sub('[!@#.,/\';]', '', orig_full_text).lower()
+    word_list = cleaned_full_text.split()
     word_count = len(word_list)
 
     word_dict = {}
@@ -24,11 +25,30 @@ def count(request):
 
     sorted_words = sorted(word_dict.items(), key=operator.itemgetter(1), reverse=True)
 
+    def count_char(original_full_text, test_char):
+        num_chars = 0
+        for char in original_full_text:
+            if char == test_char:
+                num_chars += 1
+        return num_chars
+
+    letter_count = []
+    if len(letter_count) <= len(cleaned_full_text):
+        for test_char in ALPHABET:
+            char_count = count_char(cleaned_full_text, test_char)
+            perc = 100 * count_char(cleaned_full_text,
+                                    test_char) / len(cleaned_full_text)
+            letter_count_str = f"""{test_char} appeared {char_count}
+            {'time' if char_count==1 else 'times'}
+            constituting an appearance rate of {round(perc,2)}%"""
+            letter_count.append(letter_count_str.strip('\n'))
+
     context = {
         'orig_full_text': '"' + orig_full_text + '"',
-        'context': context,
+        'cleaned_full_text': cleaned_full_text,
         'word_count': word_count,
         'sorted_words': sorted_words,
+        'letter_count': letter_count,
     }
 
     return render(request, 'wordcount/count.html', context)
