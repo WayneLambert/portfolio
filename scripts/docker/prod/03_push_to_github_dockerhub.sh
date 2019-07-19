0c43d61688b5e56871c88b285286c289de5570ca
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-07-19 21:42:18 +0100

    Adjust Docker config for production

--- scripts/docker/prod/03_push_to_github_dockerhub.sh
+++ scripts/docker/prod/03_push_to_github_dockerhub.sh
@@ -3,18 +3,20 @@
 # These commands need to be run locally from the 'src' directory
 
 # Push to GitHub: https://github.com/WayneLambert/portfolio
+git add .
+git commit -m "Adjust Docker config for production"
 git push -u origin master
 
 # Login to Docker Hub
 docker login --username=waynelambert --email=wayne.a.lambert@gmail.com
 
 # Tag all of the images required to form the full repository's build
-docker tag <IMAGE ID> waynelambert/portfolio:web_prod
-docker tag <IMAGE ID> waynelambert/portfolio:nginx_prod
-docker tag <IMAGE ID> waynelambert/portfolio:postgres_prod
+docker tag aaefe82913fe waynelambert/portfolio:201907192100_web_prod
+docker tag cae168d980a6 waynelambert/portfolio:201907192100_nginx_prod
+docker tag 596fd60351ca waynelambert/portfolio:201907192100_postgres_prod
 
 # Push to Docker Hub: https://cloud.docker.com/repository/docker/waynelambert/portfolio
 # DANGER: Ensure that the Docker Hub repo is private
-docker push waynelambert/portfolio:web_prod
-docker push waynelambert/portfolio:nginx_prod
-docker push waynelambert/portfolio:postgres_prod
+docker push waynelambert/portfolio:201907192100_web_prod
+docker push waynelambert/portfolio:201907192100_nginx_prod
+docker push waynelambert/portfolio:201907192100_postgres_prod
--- scripts/docker/prod/04_docker_run_on_linode.sh
+++ scripts/docker/prod/04_docker_run_on_linode.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Now the project has been pushed up onto Docker Hub, run the following:
-docker run waynelambert/portfolio:web_prod
+git clone https://github.com/WayneLambert/portfolio.git
+docker pull waynelambert/portfolio:201907192100_web_prod
+docker pull waynelambert/portfolio:201907192100_nginx_prod
+docker pull waynelambert/portfolio:201907192100_postgres_prod
