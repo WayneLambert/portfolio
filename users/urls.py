1f9b6397ed78b148284bf789c859b66cc026c93c
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-08-01 21:02:44 +0100

    Change configuration settings for SES for Heroku

--- users/urls.py
+++ users/urls.py
@@ -19,7 +19,9 @@ urlpatterns = [
     path(
         'password-reset/',
         auth_views.PasswordResetView.as_view(
-            template_name='users/password_reset_form.html'),
+            template_name='users/password_reset_form.html',
+            email_template_name='users/password_reset_email.html',
+            subject_template_name="users/password_reset_subject.txt",),
         name='password_reset_form'
     ),
     path(
