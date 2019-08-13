5fd28d5a63c599e8815a1350bf715f7059cae1c1
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-08-13 22:35:24 +0100

    Adjust count template

--- ab_back_end/context_processors.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings
-
-
-def get_ga_tracking_id(request):
-    return {'ga_tracking_id': settings.GA_TRACKING_ID}
--- ab_back_end/settings.py
+++ ab_back_end/settings.py
@@ -162,8 +162,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # Custom processors
-                'ab_back_end.context_processors.ga_tracking_id',
             ],
         },
     },
