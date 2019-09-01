10a0a5a8a8b7d64f3cff0d9a91275bcd09dcbb5f
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-09-01 22:47:31 +0100

    Add more tests to blog app

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -2,12 +2,14 @@
 import pytest
 from django.contrib.auth.models import AnonymousUser
 from django.urls import reverse
+from hypothesis import given
+from hypothesis.strategies import text
 import blog.views as blog_views
 pytestmark = pytest.mark.django_db  # Request database access
 
 
 def test_home(factory):
-    """ Determines whether any user can access complete list of posts """
+    """ Asserts any user can access complete list of posts """
     path = reverse('blog-django')
     request = factory.get(path)
     response = blog_views.home(request)
@@ -16,8 +18,8 @@ def test_home(factory):
 
 
 class TestPostListView:
-    """ Determines whether any user can access complete list of posts """
     def test_post_list_view(self, factory):
+        """ Asserts any user can access complete list of posts """
         path = reverse('blog-django')
         request = factory.get(path)
         response = blog_views.PostListView.as_view()(request)
@@ -25,11 +27,8 @@ class TestPostListView:
 
 
 class TestUserPostListView:
-    """
-    Determines whether any user can access list of
-    filtered posts authored by username parameter
-    """
     def test_user_post_list_view(self, factory, user):
+        """ Asserts any user can access list of posts authored by username """
         kwargs = {'username': user.username}
         path = reverse('user-posts', kwargs=kwargs)
         request = factory.get(path)
@@ -38,11 +37,8 @@ class TestUserPostListView:
 
 
 class TestCategoryPostListView:
-    """
-    Determines whether any user can access list of
-    filtered posts belonging to a given category
-    """
     def test_category_post_list_view(self, factory, category):
+        """ Asserts any user can access list of posts in a given category """
         kwargs = {'slug': category.slug}
         path = reverse('category-posts', kwargs=kwargs)
         request = factory.get(path)
@@ -51,9 +47,8 @@ class TestCategoryPostListView:
 
 
 class TestPostDetailView:
-    """ Determines whether any user can access a single post detail view """
-
     def test_post_detail_view(self, factory, post):
+        """ Asserts any user can access a single post detail view """
         kwargs = {'slug': post.slug}
         path = reverse('post-detail', kwargs=kwargs)
         request = factory.get(path)
@@ -62,25 +57,91 @@ class TestPostDetailView:
 
 
 class TestPostCreateView:
-    def test_post_create_view_logged_in(self, factory, post, user):
-        """
-        Asserts logged in user can access the update view of a single post
-        """
+    def test_post_create_view_logged_in(self, factory, user):
+        """ Asserts logged in user can create a post """
+        path = reverse('post-create')
+        request = factory.get(path)
+        request.user = user
+        response = blog_views.PostCreateView.as_view()(request)
+        assert response.status_code == 200, 'Should be callable by logged in user'
+
+    def test_post_create_view_logged_out(self, factory):
+        """ Asserts logged out user cannot create a post """
+        path = reverse('post-create')
+        request = factory.get(path)
+        request.user = AnonymousUser()
+        response = blog_views.PostCreateView.as_view()(request)
+        assert response.status_code != 200, 'Should not be callable by an anonymous user'
+        assert 'login/?next=' in response.url, 'Should redirect anonymous user to login screen'
+
+
+class TestPostUpdateView:
+    def test_post_update_view_logged_in_GET(self, factory, post):
+        """ Asserts logged in user can access the update view of a single post """
         kwargs = {'slug': post.slug}
         path = reverse('post-update', kwargs=kwargs)
         request = factory.get(path)
-        request.user = user
-        response = blog_views.PostCreateView.as_view()(request, **kwargs)
+        request.user = post.author
+        response = blog_views.PostUpdateView.as_view()(request, **kwargs)
         assert response.status_code == 200, 'Should be callable by logged in user'
 
-    def test_post_create_view_logged_out(self, factory, post):
-        """
-        Asserts logged out user cannot access the update view of a single post
-        """
+    def test_post_update_view_logged_in_POST(self, factory, post):
+        """ Asserts logged in user can update the post """
+        kwargs = {'slug': post.slug}
+        path = reverse('post-detail', kwargs=kwargs)
+        request = factory.post(path)
+        request.user = post.author
+        response = blog_views.PostUpdateView.as_view()(request, **kwargs)
+        assert response.status_code == 200, 'Should redirect user upon update'
+
+    def test_post_update_view_logged_out(self, factory, post):
+        """ Asserts logged out user cannot access the update view of a single post """
         kwargs = {'slug': post.slug}
         path = reverse('post-update', kwargs=kwargs)
         request = factory.get(path)
         request.user = AnonymousUser()
-        response = blog_views.PostCreateView.as_view()(request, **kwargs)
-        assert response.status_code != 200, 'Should not be callable by an anonymous out user'
+        response = blog_views.PostUpdateView.as_view()(request, **kwargs)
+        assert response.status_code != 200, 'Should not be callable by an anonymous user'
+        assert response.status_code == 302, 'Should redirect user'
         assert 'login/?next=' in response.url, 'Should redirect anonymous user to login screen'
+
+
+class TestPostDeleteView:
+    def test_post_delete_view_logged_in_GET(self, factory, post):
+        """ Asserts logged in user can access the delete view of a single post """
+        kwargs = {'slug': post.slug}
+        path = reverse('post-delete', kwargs=kwargs)
+        request = factory.get(path)
+        request.user = post.author
+        response = blog_views.PostDeleteView.as_view()(request, **kwargs)
+        assert response.status_code == 200, 'Should be callable by logged in user'
+
+    def test_post_delete_view_logged_in_POST(self, factory, post):
+        """ Asserts logged in user can delete the post """
+        kwargs = {'slug': post.slug}
+        path = reverse('post-delete', kwargs=kwargs)
+        request = factory.post(path)
+        request.user = post.author
+        response = blog_views.PostDeleteView.as_view()(request, **kwargs)
+        assert response.status_code == 302, 'Should redirect user to blog home page on deletion'
+        assert '/blog/' in response.url, '`blog` should appear in url following redirect'
+
+    def test_post_delete_view_logged_out(self, factory, post):
+        """ Asserts logged out user cannot access the delete view of a single post """
+        kwargs = {'slug': post.slug}
+        path = reverse('post-delete', kwargs=kwargs)
+        request = factory.get(path)
+        request.user = AnonymousUser()
+        response = blog_views.PostDeleteView.as_view()(request, **kwargs)
+        assert response.status_code != 200, 'Should not be callable by an anonymous user'
+        assert response.status_code == 302, 'Should redirect user'
+        assert 'login/?next=' in response.url, 'Should redirect anonymous user to login screen'
+
+
+class TestSearchResultsView:
+    def test_search_results_view(self, factory):
+        """ Asserts user can retrieve search results of post(s) """
+        path = f"(reverse('search-results'), {'?=test'})"
+        request = factory.get(path)
+        response = blog_views.PostDeleteView.as_view()(request)
+        assert response.status_code == 200, 'Should be callable by logged in user'
