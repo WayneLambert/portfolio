0b782835e100ca9f48d719a1a1b9be8279e68338
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-09-02 21:09:39 +0100

    Adjust Churchill and Gettysburg speeches

--- scraping/churchill_speech.py
+++ scraping/churchill_speech.py
@@ -3,22 +3,19 @@ from django.shortcuts import render
 import requests
 # pylint: disable=invalid-name
 
-URL = "https://www.goodreads.com/quotes/55276-i-have-nothing-to-offer-but-blood-toil-tears-and"
-
-# Fetch the content from the url, using the requests library
-page_response = requests.get(URL, timeout=5)
-
-# Parse the url content
-page_content = BeautifulSoup(page_response.content, "html.parser")
-
 
 def get_churchill_speech(request):
     """
     Retrieves Winston Churchill's first speech as UK prime minister from the goodreads.com
     website removing any new line escape characters returned from the parsed HTML.
     """
+
+    URL = "https://www.goodreads.com/quotes/55276-i-have-nothing-to-offer-but-blood-toil-tears-and"
+    page_response = requests.get(URL, timeout=5)
+    page_content = BeautifulSoup(page_response.content, "html.parser")
     speech_text = str(page_content.findChildren
                       ('h1')).split('\n')[1].split(';<br/>')[0].strip() + '."'
+
     return render(
         request,
         'churchill_speech.html',
--- scraping/gettysburg_speech.py
+++ scraping/gettysburg_speech.py
@@ -3,19 +3,16 @@ from django.shortcuts import render
 import requests
 # pylint: disable=invalid-name
 
-URL = "https://www.goodreads.com/work/quotes/4694-the-illustrated-gettysburg-address"
-
-# Fetch the content from the url, using the requests library
-page_response = requests.get(URL, timeout=5)
-
-# Parse the url content
-page_content = BeautifulSoup(page_response.content, "html.parser")
 
 def get_gettysburg_speech(request):
     """
     Retrieves the Gettysburg address from the goodreads.com website removing
     any new line escape characters returned from the parsed HTML.
     """
+
+    URL = "https://www.goodreads.com/work/quotes/4694-the-illustrated-gettysburg-address"
+    page_response = requests.get(URL, timeout=5)
+    page_content = BeautifulSoup(page_response.content, "html.parser")
     text_content = []
     split_string = []
     for i in range(0, 2):
