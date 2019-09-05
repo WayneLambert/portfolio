6d31c5e0685034c0563bef7c42d9e6048e2cf1dc
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-09-05 21:20:12 +0100

    Add script to determine if words in text file are abecedarian words

---
+++ countdown/is_abecedarian.py
@@ -0,0 +1,29 @@
+"""
+The following function returns True if the word passed as input is an
+abecedarian word. That is a word where the each letter in the word is a
+subsequent letter in the alphabet. 'Ant' would be a simple example.
+"""
+
+
+def is_string_abecederian(word: str) -> bool:
+    max_letter = ''
+    letters_tested = 0
+    for letter in word.lower():
+        if letter < max_letter:
+            return False
+        else:
+            max_letter = letter
+            letters_tested += 1
+    if letters_tested == len(word):
+        return True
+
+results = []
+with open('countdown/words.txt', 'r') as test_file:
+    for line in test_file:
+        result = False
+        tested_word = line.replace('\n', '')
+        if is_string_abecederian(tested_word):
+            results.append(tested_word)
+
+print(f"There are {len(results)} abecedarian words in the file. These are: \n")
+print(results)
