45b7bbf19647a2b7ead84a267258519c2a1d3a12
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-09-09 18:16:58 +0100

    Remove old 'countdown' app from git tracking

--- countdown/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from countdown import views as countdown_views
-
-
-urlpatterns = [
-    path('selection/', countdown_views.selection_screen, name='selection'),
-    path('game/', countdown_views.game_screen, name='game'),
-    path('results/', countdown_views.results_screen, name='results'),
-]
