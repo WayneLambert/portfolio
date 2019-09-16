07c690094e22af007d675e94dca7aa9e9b61868f
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-09-16 21:30:49 +0100

    Adjust wsgi.py

--- ab_back_end/wsgi.py
+++ ab_back_end/wsgi.py
@@ -1,10 +1,3 @@
-"""
-WSGI config for ab_back_endproject.
-It exposes the WSGI callable as a module-level variable named ``application``.
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
