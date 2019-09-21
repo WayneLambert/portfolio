e6420681d677e1c7c2aaed0fa326fc7c95bd9efb
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-09-21 22:15:02 +0100

    Develop 'countdown letters' app
    Add validation that the entry exists in the API. Add more accurate lookup of definition if exact word is not found. This is based upon looking up the lemma variant of the word

--- countdown_letters/views.py
+++ countdown_letters/views.py
@@ -1,6 +1,7 @@
 import os
 from random import choices, random
 from urllib.parse import urlencode
+
 from django.conf import settings
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -12,10 +13,20 @@ from countdown_letters.forms import LetterSelectionForm, SelectedLettersForm
 OD_API_BASE_URL = os.environ['OD_API_BASE_URL']
 OD_APPLICATION_ID = os.environ['OD_APPLICATION_ID']
 OD_APPLICATION_KEY_1 = os.environ['OD_APPLICATION_KEY_1']
+LANGUAGE = 'en-gb'
+headers = {
+    "app_id": OD_APPLICATION_ID,
+    "app_key": OD_APPLICATION_KEY_1,
+}
 
 MAX_GAME_LETTERS = 9
 
 def get_letters_chosen(num_vowels: int) -> str:
+    """
+    To give an appropriate proportion of the various vowels and consonants
+    within the randomised game selections, there are:
+    """
+
     WEIGHTED_VOWELS = list(
         'AAAAAAAAAAAAAAAEEEEEEEEEEEEEEEEEEEEEIIIIIIIIIIIIIOOOOOOOOOOOOOUUUUU')
     WEIGHTED_CONSONANTS = list(
@@ -81,7 +92,7 @@ def game_screen(request):
     return render(request, 'countdown_letters/game.html', context)
 
 
-def get_words():
+def get_words() ->tuple:
     words_list = []
     words_filename = os.path.join(settings.BASE_DIR, 'countdown_letters/words.txt')
     with open(words_filename, 'r') as words_file:
@@ -92,8 +103,12 @@ def get_words():
     return words
 
 
-def get_longest_possible_word(words: tuple, letters: str) -> str:
-    """ Returns the longest word in words.txt from the selected letters. """
+def get_shortlisted_words(words: tuple, letters: str) -> dict:
+    """
+    Returns a shortlist of the accumulatively gathered longest words
+    in the running order of cycling through words.txt
+    """
+    shortlisted_words = {}
     cumulative_max_letter_count = 0
     letters_in_selection = list(letters)
     for tested_word in words:
@@ -102,17 +117,28 @@ def get_longest_possible_word(words: tuple, letters: str) -> str:
             common_letters = set(letters_in_selection).intersection(
                 letters_in_tested_word)
             letter_count = len(common_letters)
-            if letter_count > cumulative_max_letter_count:
+            if letter_count >= cumulative_max_letter_count:
                 if len(tested_word) == len(common_letters):
                     cumulative_max_letter_count = letter_count
-                    longest_possible_word = tested_word
-    return longest_possible_word.upper()
+                    shortlisted_words[tested_word] = cumulative_max_letter_count
+    return shortlisted_words
 
 
-def is_valid_word(answer: str) -> bool:
-    words_list = get_words()
-    if answer.lower() in words_list:
+def is_in_oxford_api(word: str) ->bool:
+    url = f'{OD_API_BASE_URL}{"lemmas/"}{LANGUAGE}{"/"}{word.lower()}'
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
         return True
+    else:
+        return False
+
+
+def get_longest_possible_word(shortlisted_words: dict) -> str:
+    sorted_list = sorted(shortlisted_words.items(), reverse=True)
+    for item in sorted_list:
+        if is_in_oxford_api(item[0]):
+            longest_possible_word = item[0]
+            return longest_possible_word.upper()
 
 
 def is_eligible_answer(answer: str, letters: str) -> bool:
@@ -134,33 +160,52 @@ def get_game_score(word_len: int) -> int:
     else:
         return word_len
 
-def lookup_definition(request, word_to_lookup: str) -> dict:
-    """ Retrieve dictionary definition of winning word using 'Oxford Dictionaries' API """
-    LANGUAGE = 'en-gb'
-    url = f'{OD_API_BASE_URL}{LANGUAGE}{"/"}{word_to_lookup.lower()}'
-    headers = {
-        "app_id": OD_APPLICATION_ID,
-        "app_key": OD_APPLICATION_KEY_1,
-    }
 
-    response = requests.get(url, headers=headers)
-    if response.status_code == 404:
-        definition_found = False
-        definition = ''
-        word_class = ''
-    else:
-        definition_found = True
-        response_json = response.json()
-        definition = response_json['results'][0]['lexicalEntries'][0]
+def get_lemmas_response_json(word: str) ->dict:
+    lemmas_url = f"{OD_API_BASE_URL}{'lemmas/'}{LANGUAGE}{'/'}{word.lower()}"
+    lemmas_response = requests.get(lemmas_url, headers=headers)
+    lemmas_response_json = lemmas_response.json()
+    return lemmas_response_json
+
+
+def get_alt_word(word: str) -> str:
+    """
+    Retrieve alternative word to the exact winning word. The singular form of the
+    word may be the referenced word in the Oxford definitions API. """
+    lemmas_json = get_lemmas_response_json(word)
+    alt_word_lookup = lemmas_json['results'][0]
+    alt_word_lookup = alt_word_lookup['lexicalEntries'][0]
+    alt_word_lookup = alt_word_lookup['inflectionOf'][0]['id']
+    return alt_word_lookup
+
+
+def lookup_definition(word: str) -> dict:
+    """
+    Retrieve dictionary definition of winning word using 'Oxford Dictionaries' API.
+    An alternative form of the word is used if the Oxford API does not return a
+    definition for the exact match of the winning form. In this case, it return a
+    result from the lemmas query.
+    """
+    definitions_url = f'{OD_API_BASE_URL}{"entries/"}{LANGUAGE}{"/"}{word.lower()}'
+    response = requests.get(definitions_url, headers=headers)
+    if response.status_code == 200:
+        definitions_response_json = response.json()
+        definition = definitions_response_json['results'][0]['lexicalEntries'][0]
         word_class = definition['lexicalCategory']['text']
         try:
             definition = definition['entries'][0]['senses'][0]['definitions'][0].capitalize()
         except KeyError:
-            definition = f"""The definition for {word_to_lookup} cannot be found
+            definition = f"""The definition for '{word}' cannot be found
                              in the Oxford Dictionary API."""
+        alt_word_lookup = word
+    else:
+        alt_word_lookup = get_alt_word(word)
+        definition = lookup_definition(alt_word_lookup)
+        lemmas_json = get_lemmas_response_json(word)
+        word_class = lemmas_json['results'][0]['lexicalEntries'][0]['lexicalCategory']['text']
 
     definition_result = {
-        'definition_found': definition_found,
+        'alt_word_lookup': alt_word_lookup,
         'definition': definition,
         'word_class': word_class,
     }
@@ -170,7 +215,7 @@ def lookup_definition(request, word_to_lookup: str) -> dict:
 
 def get_result(player_word: str, comp_word: str) -> str:
     if len(player_word) > len(comp_word):
-        return 'Player wins'
+        return 'You win'
     if len(player_word) < len(comp_word):
         return 'Susie wins'
     else:
@@ -182,7 +227,7 @@ def results_screen(request):
     listed_words = get_words()
 
     players_word = request.GET['players_word']
-    valid_word = is_valid_word(players_word)
+    valid_word = is_in_oxford_api(players_word)
     eligible_answer = is_eligible_answer(players_word, letters_chosen)
     if valid_word and eligible_answer:
         player_word_len = len(players_word)
@@ -191,11 +236,12 @@ def results_screen(request):
         player_word_len = 0
         player_score = 0
 
-    comp_word = get_longest_possible_word(listed_words, letters_chosen)
+    shortlisted_words = get_shortlisted_words(listed_words, letters_chosen)
+    comp_word = get_longest_possible_word(shortlisted_words)
     comp_word_len = len(comp_word)
     comp_score = get_game_score(comp_word_len)
     winning_word = comp_word if comp_word_len > player_word_len else players_word
-    definition = lookup_definition(request, winning_word)
+    definition_result = lookup_definition(winning_word)
     result = get_result(players_word, comp_word)
 
     context = {
@@ -208,7 +254,7 @@ def results_screen(request):
         'comp_word_len': comp_word_len,
         'comp_score': comp_score,
         'winning_word': winning_word,
-        'definition': definition,
+        'definition_result': definition_result,
         'result': result,
     }
 
