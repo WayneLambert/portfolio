06bdcb3eff4de97fe66b621f81297dca43c874d2
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-12-02 21:28:02 +0000

    Develop 'Scraping' app

--- scraping/urls.py
+++ scraping/urls.py
@@ -4,7 +4,6 @@ from scraping import (
     gettysburg_speech,
     jokes,
     referendum,
-    word_of_the_day
 )
 from scraping import views as scraping_views
 
@@ -16,8 +15,6 @@ urlpatterns = [
     path('gettysburg-speech/', gettysburg_speech.get_gettysburg_speech,
          name='gettysburg-speech'),
     path('jokes/', jokes.get_joke, name='jokes'),
-    path('word-of-the-day/', word_of_the_day.get_word_of_the_day,
-         name='word-of-the-day'),
     path('referendum/', referendum.get_referendum_results,
          name='referendum'),
 ]
