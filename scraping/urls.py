3d6851e78048dee4e7224c0cb162c7f908fc4dfb
Wayne Lambert <wayne.a.lambert@gmail.com>
2020-06-10 14:13:41 +0100

    Remove scraping of jokes from portfolio

--- scraping/urls.py
+++ scraping/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from scraping import churchill_speech, gettysburg_speech, jokes, referendum
+from scraping import churchill_speech, gettysburg_speech, referendum
 from .views import scraping_options_list
 
 app_name = 'scraping'
@@ -10,6 +10,5 @@ urlpatterns = [
     path('churchill-speech/', churchill_speech.get_churchill_speech, name='churchill_speech'),
     path('gettysburg-speech/',
          gettysburg_speech.get_gettysburg_speech, name='gettysburg_speech'),
-    path('jokes/', jokes.get_joke, name='jokes'),
     path('referendum/', referendum.get_referendum_results, name='referendum'),
 ]
