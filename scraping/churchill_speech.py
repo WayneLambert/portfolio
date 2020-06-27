b931fb911a2e988eafe4ffeb75d25c9560e2fe00
Wayne Lambert <wayne.a.lambert@gmail.com>
2020-06-27 18:08:25 +0100

    Resolve dark mode issues in 'scraping' app

--- scraping/churchill_speech.py
+++ scraping/churchill_speech.py
@@ -1,6 +1,7 @@
+import requests
 from bs4 import BeautifulSoup
 from django.shortcuts import render
-import requests
+
 # pylint: disable=invalid-name
 
 
@@ -13,11 +14,9 @@ def get_churchill_speech(request):
     URL = "https://www.goodreads.com/quotes/55276-i-have-nothing-to-offer-but-blood-toil-tears-and"
     page_response = requests.get(URL, timeout=5)
     page_content = BeautifulSoup(page_response.content, "html.parser")
-    speech_text = str(
+    churchill_speech = str(
         page_content.findChildren('h1')).split('\n')[1].split(';<br/>')[0].strip() + '."'
 
-    return render(
-        request,
-        'churchill_speech.html',
-        {'churchill_speech': speech_text}
-    )
+    context = {'churchill_speech': churchill_speech}
+
+    return render(request, 'churchill_speech.html', context)
--- scraping/gettysburg_speech.py
+++ scraping/gettysburg_speech.py
@@ -1,6 +1,7 @@
+import requests
 from bs4 import BeautifulSoup
 from django.shortcuts import render
-import requests
+
 # pylint: disable=invalid-name
 
 
@@ -13,15 +14,11 @@ def get_gettysburg_speech(request):
     URL = "https://www.goodreads.com/work/quotes/4694-the-illustrated-gettysburg-address"
     page_response = requests.get(URL, timeout=5)
     page_content = BeautifulSoup(page_response.content, "html.parser")
-    text_content = []
-    split_string = []
-    for i in range(0, 2):
-        paragraphs = page_content.find_all('div', attrs={"class": "quoteText"})[i].text
+    text_content, split_string = [], []
+    for num in range(0, 2):
+        paragraphs = page_content.find_all('div', attrs={"class": "quoteText"})[num].text
         text_content.append(paragraphs)
     split_string = text_content[1].strip('\n').strip().split('\n')
     gettysburg_speech = split_string[0]
-    return render(
-        request,
-        'gettysburg_speech.html',
-        {'gettysburg': gettysburg_speech}
-    )
+    context = {'gettysburg': gettysburg_speech}
+    return render(request, 'gettysburg_speech.html', context)
