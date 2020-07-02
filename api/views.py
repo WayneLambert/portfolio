94db668de2b0b03abc60c3860278f03df1ee4f1f
Wayne Lambert <wayne.a.lambert@gmail.com>
2020-07-02 16:41:15 +0100

    Improve query efficiency for API

--- api/views.py
+++ api/views.py
@@ -7,19 +7,19 @@ from .serializers import CategorySerializer, PostSerializer
 
 class CategoryListAPIView(generics.ListAPIView):
     """ API endpoint that enables category list to be viewed. """
-    queryset = Category.objects.all()
+    queryset = Category.objects.all().prefetch_related('posts')
     serializer_class = CategorySerializer
     lookup_fields = ('name')
 
 
 class PostListAPIView(generics.ListAPIView):
     """ API endpoint that enables post list to be viewed. """
-    queryset = Post.objects.all()
+    queryset = Post.objects.prefetch_related('categories').select_related('author__user')
     serializer_class = PostSerializer
     lookup_fields = ('title')
 
 
 class PostDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     """ API endpoint that enables post instances to be viewed or edited. """
-    queryset = Post.objects.all()
+    queryset = Post.objects.prefetch_related('categories').select_related('author__user')
     serializer_class = PostSerializer
