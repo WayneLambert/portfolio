5ab920a9295a06988a133138e05630b48510aeae
Wayne Lambert <wayne.a.lambert@gmail.com>
2020-07-10 22:03:06 +0100

    Update 'contacts' app

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import contact, contact_submitted
+from .views import ContactFormView, ContactSubmittedView
 
 app_name = 'contacts'
 
 urlpatterns = [
-    path('', contact, name='contact'),
-    path('submitted/', contact_submitted, name='submitted'),
+    path('', ContactFormView.as_view(), name='contact'),
+    path('submitted/', ContactSubmittedView.as_view(), name='submitted'),
 ]
