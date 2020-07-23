a4a587086238eb29e9a47fc30db88578c487763d
Wayne Lambert <wayne.a.lambert@gmail.com>
2020-07-23 15:21:03 +0100

    Change config
    Test project with Bandit

--- ab_back_end/settings.py
+++ ab_back_end/settings.py
@@ -1,6 +1,7 @@
 import os
+from pathlib import Path
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
