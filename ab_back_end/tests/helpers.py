f96b8c219c27d676ef9a02afa59744cc449161b1
Wayne Lambert <wayne.a.lambert@gmail.com>
2020-08-03 14:23:20 +0100

    Update PyTest to version 6.0.1
    The update to PyTest >6.0 introduced some breaking changes due to feature deprecations. Unit tests have been re-ran all and have passed.
    
    Adds the following functionality:
    
    - Updated Pytest to version 6.0.1 + Hypothesis
    - Amended positional arguments for fixtures to named ones
    - Amended `pytest.ini` file to remove unknown configuration options
    - Added docstrings to global `helpers` module
    
    See also: #40

--- ab_back_end/tests/helpers.py
+++ ab_back_end/tests/helpers.py
@@ -1,16 +1,25 @@
+""" A suite of helper structures and functions that support global
+fixtures to simulate frequently used testing scenarios.
+"""
+
 import pytest
+
 from django.contrib.auth import get_user_model
 
-# Helper function to create a logged in and logged out user
+
+# Helper structures to emulate a logged in and logged out user
 user_types = ('logged_in_user', 'logged_out_user')
 lilo_users = (
-    get_user_model()('li_user', pytest.fixture('li_user')),
-    get_user_model()('lo_user', pytest.fixture('lo_user')),
+    get_user_model()('li_user', pytest.fixture(name='li_user')),
+    get_user_model()('lo_user', pytest.fixture(name='lo_user')),
 )
 
 
-# Helper function to add Middleware to a request
 def add_middleware_to_request(request, middleware_class):
+    """
+    Supports a testing scenario which simulates an active middleware
+    session within the lifecycle of a request.
+    """
     middleware = middleware_class()
     middleware.process_request(request)
     request.session.save()
