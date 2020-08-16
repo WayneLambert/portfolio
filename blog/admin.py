30d48fa2b94f251c5b147dc19df8571b68b2baf5
Wayne Lambert <wayne.a.lambert@gmail.com>
2020-08-16 20:57:05 +0100

    Develop Admin Panel
    Addresses the following:
    
    - Adds logo to the admin panel
    - Adds calculations for number of posts by category
    - Adds image preview for posts and user profiles
    - Removes access to unused standard models
    - Updates config for pre-commits and `pyproject.toml`

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,44 @@
 from django.contrib import admin
-from blog.models import Category, Post
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+
+from .models import Category
+from .models import Post
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'created_date', )
+    list_display = (
+        'name', 'slug', 'post_count', 'created_date', )
     prepopulated_fields = {'slug': ('name',)}
+
+    def get_queryset(self, request):
+        """ Calculation number of published posts by given category """
+        queryset = super().get_queryset(request)
+        queryset = queryset.filter(
+            posts__status=1).annotate(_post_count=Count('posts', distinct=True))
+        return queryset
+
+    def post_count(self, obj):
+        return obj._post_count
 
 
 class PostAdmin(admin.ModelAdmin):
+    fields = (
+        'title', 'slug', 'content', 'reference_url', 'status', 'author',
+        'categories', 'image', 'post_image'
+    )
     list_display = ('title', 'status', 'publish_date', 'updated_date')
     list_filter = ('status', 'categories', 'author')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'updated_date'
+    readonly_fields = ['post_image', ]
+
+    def post_image(self, obj):
+        img_width = obj.image.width * 0.25
+        img_height = obj.image.height * 0.25
+        return mark_safe(  # nosec
+            f"<img src='{obj.image.url}' width='{img_width}' height='{img_height}' />")
 
 
 admin.site.register(Category, CategoryAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.utils.safestring import mark_safe
 
 from .models import Profile
 
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'author_view',)
+    readonly_fields = ('profile_picture_image', )
+
+    def profile_picture_image(self, obj):
+        img_width = obj.profile_picture.width
+        img_height = obj.profile_picture.height
+        url = obj.profile_picture.url
+        return mark_safe(  # nosec
+            f"<img src='{url}' width='{img_width}' height='{img_height}' />")
 
 
 admin.site.register(Profile, ProfileAdmin)
+admin.site.unregister(Group)
