c5014f983a5514afb02d35409ea2b761150e6785
Wayne Lambert <wayne.a.lambert@gmail.com>
2020-12-14 16:46:06 +0000

    Adjust URL patterns
    Addresses the following:
    
    - Corrects URL for text analysis endpoints to be hyphenated rather than
      underscored
    - Tidies up some URL patterns

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -14,10 +14,8 @@ urlpatterns = [
 
 # Custom Login and Password Reset Process
 urlpatterns += [
-    path('login/',
-         auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/',
-         auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
              template_name='users/password_reset_form.html',
