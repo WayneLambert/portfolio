cf98d70d8c6e260c801f723a3eb401b152b7c63f
Wayne Lambert <wayne.a.lambert@gmail.com>
2021-08-06 13:37:57 +0100

    Set up models
    Addresses the following:
    
    - Creates a new `EmailToken` model
    - Implements the use of the Django Cryptography package to encrypt the
      `challenge_token` field
    - Performs a series of migrations to amend the model to be suitable
    - Makes minor adjustments to the exisiting `Profile` model for
      readability/maintainability
    
    Contributes towards: #107

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
 
+from django_cryptography.fields import encrypt
+from django_otp.util import random_hex
+
+from apps.users.utils import get_challenge_expiration_timestamp, token_validator
+
 
 class Profile(models.Model):
 
@@ -10,14 +15,16 @@ class Profile(models.Model):
         USERNAME = 0
         FULL_NAME = 1
 
-    user = models.OneToOneField(
-        get_user_model(), primary_key=True, related_name='user', on_delete=models.CASCADE)
     slug = models.SlugField(max_length=255, unique=True)
     author_view = models.IntegerField(choices=AuthorView.choices, default=0)
     profile_picture = models.ImageField(
         default='profile_pics/default-user.jpg', upload_to='profile_pics', max_length=200)
     created_date = models.DateTimeField(auto_now_add=True, editable=False)
     updated_date = models.DateTimeField(auto_now=True)
+
+    # Relationship Fields
+    user = models.OneToOneField(
+        get_user_model(), primary_key=True, related_name='user', on_delete=models.CASCADE)
 
     @property
     def full_name(self):
@@ -49,3 +56,26 @@ class Profile(models.Model):
         if not self.slug.strip():
             self.slug = slugify(self.user.username)
         super(Profile, self).save(*args, **kwargs)
+
+
+class EmailToken(models.Model):
+
+    challenge_email_address = models.EmailField()
+    challenge_token = encrypt(models.CharField(
+        max_length=40, default=random_hex, validators=[token_validator]))
+    challenge_generation_timestamp = models.DateTimeField(
+        null=True, auto_now_add=True, editable=False)
+    challenge_expiration_timestamp = models.DateTimeField(
+        null=True, default=get_challenge_expiration_timestamp)
+    challenge_completed = models.BooleanField(default=False)
+    challenge_completed_timestamp = models.DateTimeField(null=True, auto_now=True)
+
+    # Relationship Fields
+    user = models.ForeignKey(
+        get_user_model(), related_name='user_email_devices', on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return f"<EmailDevice(id={self.id}, user={self.user})>"
+
+    def __str__(self):
+        return f"<EmailDevice(id={self.id}, user={self.user})>"
