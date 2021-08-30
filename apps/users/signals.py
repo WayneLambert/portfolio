b619408c1d2dbe540f0d7bf08d198bf00c5a0adb
Wayne Lambert <wayne.a.lambert@gmail.com>
2021-08-30 18:13:06 +0100

    Refactor signals within users app
    Addresses the following:
    
    - Consolidates the two signals into one signal
    - Adds a docstring to give an explanation and context for the signal
    - Makes the project import from the model relative
    
    Fixes: #115

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -2,15 +2,15 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from apps.users.models import Profile
+from .models import Profile
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_and_save_profile(sender, instance, created, **kwargs):
+    """
+    When new `User` object is created, a corresponding `Profile` object
+    is created.
+    """
     if created:
         Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
     instance.profile.save()
