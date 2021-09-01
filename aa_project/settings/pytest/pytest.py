361232d7716e15b3ed81b00070d89ad2c82b51b4
Wayne Lambert <wayne.a.lambert@gmail.com>
2021-09-01 11:40:55 +0100

    Update Django tests config for GitHub Actions
    Addresses the following:
    
    - Updates the `test.yml` config to change database settings
    - Runs the actions on `develop` and `ci` branches too
    - Runs the actions on pull requests too
    - Updates the database configuration for the database being used during
      the tests
    - Updates the secret for the `DJANGO_SETTINGS_MODULE` module within the
      secrets GitHub interface

--- aa_project/settings/pytest/pytest.py
+++ aa_project/settings/pytest/pytest.py
@@ -24,6 +24,22 @@ DATABASES = {
     }
 }
 
+# GitHub Actions Postgres
+if os.environ['GITHUB_WORKFLOW']:
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        },
+        'TEST': {
+            'NAME': 'test_portfolio_db',
+        },
+    }
+
 # Ensures tests are never sending actual emails
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 
