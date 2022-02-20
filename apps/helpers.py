28ecbb83c57ac75938219a60a41dd07491217816
Wayne Lambert <wayne.a.lambert@gmail.com>
2022-02-20 17:07:00 +0000

    Update pytest config
    Addresses the following:
    
    - Passes request object in initialisation of middleware
    - Moves config into `pyproject.toml`
    - Removes `pytest.ini` as a result

--- apps/helpers.py
+++ apps/helpers.py
@@ -9,7 +9,7 @@ def add_middleware_to_request(request, middleware_class):
     Supports a testing scenario which simulates an active middleware
     session within the lifecycle of a request.
     """
-    middleware = middleware_class()
+    middleware = middleware_class(request)
     middleware.process_request(request)
     request.session.save()
     return request
