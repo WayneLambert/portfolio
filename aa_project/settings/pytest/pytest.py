e61eba3bc6acf3d4b003f352a7ec283b7b293a76
Wayne Lambert <wayne.a.lambert@gmail.com>
2023-12-10 15:05:05 +0000

    Update packages
    Addresses the following:
    
    - Updates gunicorn
    - Updates markdown
    - Updates boto3
    - Updates pillow
    - Updates dj-database-url
    - Updates hypothesis (dev)
    - Updates some settings file with ruff formatting and noqa pragmas

--- aa_project/settings/pytest/pytest.py
+++ aa_project/settings/pytest/pytest.py
@@ -1,4 +1,4 @@
-# pylint: disable=unused-import
+# ruff: noqa: F403
 
 import os
 
@@ -9,46 +9,47 @@ from aa_project.settings.base import *
 
 # Postgres Testing Database Configuration. Uses in-memory DB
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS'),
-        'HOST': os.getenv('DB_DOCKER_POSTGRES_SERVICE'),
-        'PORT': os.getenv('DB_PORT'),
-        'TEST': {
-            'NAME': 'test_portfolio_db',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASS"),
+        "HOST": os.getenv("DB_DOCKER_POSTGRES_SERVICE"),
+        "PORT": os.getenv("DB_PORT"),
+        "TEST": {
+            "NAME": "test_portfolio_db",
         },
     }
 }
 
 # GitHub Actions Postgres
-if 'GITHUB_RUN_ID' in os.environ:
+if "GITHUB_RUN_ID" in os.environ:
     DATABASES = {
-        'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': 'github_actions',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "github_actions",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
         },
-        'TEST': {
-            'NAME': 'test_portfolio_db',
+        "TEST": {
+            "NAME": "test_portfolio_db",
         },
     }
 
 # Ensures tests are never sending actual emails
-EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
 # Ensures tests have access to a password for tests from the environment
-PYTEST_TEST_PASSWORD = os.environ['PYTEST_TEST_PASSWORD']
+PYTEST_TEST_PASSWORD = os.environ["PYTEST_TEST_PASSWORD"]
 
 # Override back to default storage since tests do not run `collectstatic`
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Eliminates warning about missing staticfiles directory
 WHITENOISE_AUTOREFRESH = True
+
 
 # A less secure password hasher used for testing to enable faster test runs
 class SimplePasswordHasher(BasePasswordHasher):
@@ -72,13 +73,11 @@ class SimplePasswordHasher(BasePasswordHasher):
         return password == hash
 
     def safe_summary(self, encoded):
-        """ This is an unsafe version """
+        """This is an unsafe version"""
         return {"algorithm": "dumb", "hash": encoded.split("$", 2)[2]}
 
 
-PASSWORD_HASHERS = (
-    'aa_project.settings.pytest.pytest.SimplePasswordHasher',
-)
+PASSWORD_HASHERS = ("aa_project.settings.pytest.pytest.SimplePasswordHasher",)
 
 # Celery Settings
 CELERY_ALWAYS_EAGER = True
